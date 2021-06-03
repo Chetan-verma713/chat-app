3bcf466a3500f8e5fe625c9eced0cd0a972a7ed8
Naveengali <naveengali80@gmail.com>
2021-06-03 15:00:26 +0530

    Made Public and private Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import { Switch } from 'react-router';
 import 'rsuite/dist/styles/rsuite-default.css';
+import Signin from './pages/Signin';
+import Home from './pages/Home';
 import './styles/main.scss';
+import PrivateRotue from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <Signin />
+      </PublicRoute>
+      <PrivateRotue path="/">
+        <Home />
+      </PrivateRotue>
+    </Switch>
+  );
 }
 
 export default App;
