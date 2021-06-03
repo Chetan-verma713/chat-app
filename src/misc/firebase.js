fc8178ded6da23cf72aa936f18ded0c242eae9a4
Naveengali <naveengali80@gmail.com>
2021-06-03 12:02:09 +0530

    Initialized FireBase Project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyAeruFobgOf4EEEIzRU9UImjFupJ-zI9b0',
+  authDomain: 'chat-web-app-fccf6.firebaseapp.com',
+  projectId: 'chat-web-app-fccf6',
+  storageBucket: 'chat-web-app-fccf6.appspot.com',
+  messagingSenderId: '891880887658',
+  appId: '1:891880887658:web:6fff5e39128be492068113',
+};
+
+const app = firebase.initializeApp(config);
