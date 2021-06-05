2c515bf582e46593fb4636f250787e50ad9ce171
Naveengali <naveengali80@gmail.com>
2021-06-05 14:19:24 +0530

    Added useMediaQuery and made DashBoard Responsive

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,18 +1,19 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 import { Button, Drawer, Icon } from 'rsuite';
-import { useModalState } from '../../misc/CustomHooks';
+import { useMediaQuery, useModalState } from '../../misc/CustomHooks';
 import Dashboard from '.';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
+  const isMobile = useMediaQuery('(max-width:992px)');
 
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" /> DashBoard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
