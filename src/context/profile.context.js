f3bb409d2b72b62389b13651bfc50ead1a858382
Naveengali <naveengali80@gmail.com>
2021-06-05 07:21:05 +0530

    Added Loader, managed realtime subs and got userdata from firebase

--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,12 +1,49 @@
-import React, { createContext, useContext, useState } from 'react';
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { auth, database } from '../misc/firebase';
 
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
-  const [profile] = useState(false);
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    let userRef;
+    const authUnSub = auth.onAuthStateChanged(authObj => {
+      if (authObj) {
+        userRef = database.ref(`/profiles/${authObj.uid}`);
+        userRef.on('value', snap => {
+          const { name, createdAt } = snap.val();
+          const data = {
+            name,
+            createdAt,
+            uid: authObj.uid,
+            email: authObj.email,
+          };
+          setProfile(data);
+          setIsLoading(false);
+        });
+      } else {
+        if (userRef) {
+          userRef.off();
+        }
+        setProfile(null);
+        setIsLoading(false);
+      }
+    });
+
+    return () => {
+      authUnSub();
+      if (userRef) {
+        userRef.off();
+      }
+    };
+  }, []);
 
   return (
-    <ProfileContext.Provider value={profile}>
+    <ProfileContext.Provider value={{ isLoading, profile }}>
       {children}
     </ProfileContext.Provider>
   );
