79048c8a406e96b240dbf7c8819f600a56b26316
Naveengali <naveengali80@gmail.com>
2021-06-05 14:37:38 +0530

    Added Signout functionality and displayed username

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { Button, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
 
-const DashBoard = () => <div>Hello</div>;
+const DashBoard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>DashBoard</Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>Hey {profile.name}</h3>
+        {/* <h4>{profile.email}</h4> */}
+      </Drawer.Body>
+
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          SignOut
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
+};
 
 export default DashBoard;
