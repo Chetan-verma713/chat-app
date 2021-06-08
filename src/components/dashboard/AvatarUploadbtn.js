c48c01fcc1f4521b6fca264d19301a8f4bc73ac0
Naveengali <naveengali80@gmail.com>
2021-06-08 09:28:29 +0530

    Added Avatar to dashboard and used name Initials

--- src/components/dashboard/AvatarUploadbtn.js
+++ src/components/dashboard/AvatarUploadbtn.js
@@ -5,6 +5,7 @@ import AvatarEditor from 'react-avatar-editor';
 import { useModalState } from '../../misc/CustomHooks';
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
+import ProfileAvatar from '../ProfileAvatar';
 
 const fileInputTypes = '.png, .jpeg, .jpg';
 
@@ -79,6 +80,12 @@ const AvatarUploadBtn = () => {
 
   return (
     <div className="mt-3 text-center">
+      <ProfileAvatar
+        src={profile.avatar}
+        name={profile.name}
+        className="width-200 height-200 img-fullsize font-huge"
+      />
+
       <div>
         <label
           htmlFor="avatar-upload"
