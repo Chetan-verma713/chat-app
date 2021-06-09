244d26ae427e811a0ab2b34b86c46099149967ae
Naveengali <naveengali80@gmail.com>
2021-06-09 07:40:50 +0530

    Created rooms Context and retrived rooms data from firebase

---
+++ src/context/rooms.context.js
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { database } from '../misc/firebase';
+import { transformToArrwithId } from '../misc/helpers';
+
+const RoomsContext = createContext();
+
+export const RoomsProvider = ({ children }) => {
+  const [rooms, setrooms] = useState(null);
+
+  useEffect(() => {
+    const roomListRef = database.ref('rooms');
+
+    roomListRef.on('value', snap => {
+      const data = transformToArrwithId(snap.val());
+      setrooms(data);
+    });
+
+    return () => {
+      roomListRef.off();
+    };
+  }, []);
+
+  return (
+    <RoomsContext.Provider value={rooms}>{children}</RoomsContext.Provider>
+  );
+};
