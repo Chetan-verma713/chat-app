911b12b1cb64f0434688dae6be3fb157fde2ca55
Naveengali <naveengali80@gmail.com>
2021-06-09 20:18:58 +0530

    Added Conditions for Chatroom display

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,5 +1,40 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
-const Chat = () => <div>Hello</div>;
+import { Loader } from 'rsuite';
+import Messages from '../../components/chat-window/messages';
+import ChatTop from '../../components/chat-window/top';
+import ChatBottom from '../../components/chat-window/bottom';
+import { useRooms } from '../../context/rooms.context';
+
+const Chat = () => {
+  const { chatId } = useParams();
+
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader centered vertical size="md" contet="Loading" speen="slow" />;
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId);
+
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
+  }
+
+  return (
+    <>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
+};
 
 export default Chat;
